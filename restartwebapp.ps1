50a3e167463cc19f8b6fe6af582b0f44917b0295
tofusgh <31666473+tofusgh@users.noreply.github.com>
2021-05-19 15:14:57 -0400

    missing auth

--- restartwebapp.ps1
+++ restartwebapp.ps1
@@ -1,4 +1,5 @@
 #restart a webapp using azure automation 
+Connect-AzAccount -Identity
 stop-azwebapp -resourcegroup 'az-dev-rg-01' -name  'kamyasapp'
 start-azwebapp -resourcegroup 'az-dev-rg-01' -name 'kamyasapp'
 
